551f82488d831fba8f0a3070203d2c47ca46e116
franciscolang <francisco.lang2209@gmail.com>
2025-06-12 00:11:21 -0300

    corrected the headless cypress run

--- cypress/e2e/userFlows.cy.js
+++ cypress/e2e/userFlows.cy.js
@@ -1,9 +1,13 @@
 describe('User flows', () => {
+  const delay_demo = 0;
+  const delay_typing = 0;
+  
   const timestamp = Date.now();
   const userEmail = `user${timestamp}@example.com`;
   const userPassword = 'Password123!';
   const alias = `user${timestamp}`;
   const recipientEmail = `recipient${timestamp}@example.com`;
+  const recipientAlias = `recipient${timestamp}`;
 
   it('registers a new user', () => {
     cy.visit('/');
@@ -12,14 +16,14 @@ describe('User flows', () => {
     cy.get('[data-testid="goto-register"]').click();
     cy.wait(300);
 
-    cy.get('[data-testid="register-email"]').type(userEmail, { delay: 80 });
-    cy.get('[data-testid="register-password"]').type(userPassword, { delay: 80 });
-    cy.get('[data-testid="register-alias"]').type(alias, { delay: 80 });
+    cy.get('[data-testid="register-email"]').type(userEmail, { delay: delay_typing });
+    cy.get('[data-testid="register-password"]').type(userPassword, { delay: delay_typing });
+    cy.get('[data-testid="register-alias"]').type(alias, { delay: delay_typing });
     cy.get('[data-testid="register-submit"]').click();
-    cy.wait(500);
+    cy.wait(delay_demo);
 
     cy.get('[data-testid="current-balance"]').should('be.visible');
-    cy.wait(500);
+    cy.wait(delay_demo);
 
     cy.get('[data-testid="logout-button"]').click();
   });
@@ -31,14 +35,14 @@ describe('User flows', () => {
     cy.get('[data-testid="goto-register"]').click();
     cy.wait(300);
 
-    cy.get('[data-testid="register-email"]').type(recipientEmail, { delay: 80 });
-    cy.get('[data-testid="register-password"]').type(userPassword, { delay: 80 });
-    cy.get('[data-testid="register-alias"]').type('Recipient', { delay: 80 });
+    cy.get('[data-testid="register-email"]').type(recipientEmail, { delay: delay_typing });
+    cy.get('[data-testid="register-password"]').type(userPassword, { delay: delay_typing });
+    cy.get('[data-testid="register-alias"]').type(recipientAlias, { delay: delay_typing });
     cy.get('[data-testid="register-submit"]').click();
-    cy.wait(500);
+    cy.wait(delay_demo);
 
     cy.get('[data-testid="current-balance"]').should('be.visible');
-    cy.wait(500);
+    cy.wait(delay_demo);
 
     cy.get('[data-testid="logout-button"]').click();
   });
@@ -47,32 +51,32 @@ describe('User flows', () => {
     cy.visit('/');
     cy.wait(300);
 
-    cy.get('[data-testid="login-email"]').type(userEmail, { delay: 80 });
-    cy.get('[data-testid="login-password"]').type(userPassword, { delay: 80 });
+    cy.get('[data-testid="login-email"]').type(userEmail, { delay: delay_typing });
+    cy.get('[data-testid="login-password"]').type(userPassword, { delay: delay_typing });
     cy.get('[data-testid="login-submit"]').click();
-    cy.wait(500);
+    cy.wait(delay_demo);
 
     cy.get('[data-testid="goto-debin"]').click();
     cy.wait(300);
-    cy.get('[data-testid="debin-amount"]').type('100', { delay: 80 });
+    cy.get('[data-testid="debin-amount"]').type('100', { delay: delay_typing });
     cy.get('[data-testid="debin-submit"]').click();
-    cy.wait(500);
+    cy.wait(delay_demo);
 
     cy.get('[data-testid="current-balance"]').should('contain', '100')
-    cy.wait(500);
+    cy.wait(delay_demo);
 
     cy.get('[data-testid="goto-transfer"]').click();
     cy.wait(300);
-    cy.get('[data-testid="transfer-recipient"]').type(recipientEmail, { delay: 80 });
-    cy.get('[data-testid="transfer-amount"]').type('5', { delay: 80 });
+    cy.get('[data-testid="transfer-recipient"]').type(recipientEmail, { delay: delay_typing });
+    cy.get('[data-testid="transfer-amount"]').type('5', { delay: delay_typing });
     cy.get('[data-testid="transfer-submit"]').click();
-    cy.wait(500);
+    cy.wait(delay_demo);
 
     cy.get('[data-testid="goto-transactions"]').click({ force: true });
     cy.wait(300);
     cy.get('[data-testid="transactions-list"]').should('exist');
     cy.get('[data-testid="transaction-item"]').its('length').should('be.greaterThan', 0);
-    cy.wait(500);
+    cy.wait(delay_demo);
 
     cy.get('[data-testid="back-home"]').click();
     cy.wait(300);
@@ -84,32 +88,32 @@ describe('User flows', () => {
     cy.visit('/');
     cy.wait(300);
 
-    cy.get('[data-testid="login-email"]').type(recipientEmail, { delay: 80 });
-    cy.get('[data-testid="login-password"]').type(userPassword, { delay: 80 });
+    cy.get('[data-testid="login-email"]').type(recipientEmail, { delay: delay_typing });
+    cy.get('[data-testid="login-password"]').type(userPassword, { delay: delay_typing });
     cy.get('[data-testid="login-submit"]').click();
-    cy.wait(500);
+    cy.wait(delay_demo);
 
     cy.get('[data-testid="goto-debin"]').click();
     cy.wait(300);
-    cy.get('[data-testid="debin-amount"]').type('10', { delay: 80 });
+    cy.get('[data-testid="debin-amount"]').type('10', { delay: delay_typing });
     cy.get('[data-testid="debin-submit"]').click();
-    cy.wait(500);
+    cy.wait(delay_demo);
 
     cy.get('[data-testid="current-balance"]').should('contain', '15');
-    cy.wait(500);
+    cy.wait(delay_demo);
 
     cy.get('[data-testid="goto-transfer"]').click();
     cy.wait(300);
-    cy.get('[data-testid="transfer-recipient"]').type(userEmail, { delay: 80 });
-    cy.get('[data-testid="transfer-amount"]').type('2', { delay: 80 });
+    cy.get('[data-testid="transfer-recipient"]').type(userEmail, { delay: delay_typing });
+    cy.get('[data-testid="transfer-amount"]').type('2', { delay: delay_typing });
     cy.get('[data-testid="transfer-submit"]').click();
-    cy.wait(500);
+    cy.wait(delay_demo);
 
     cy.get('[data-testid="goto-transactions"]').click({ force: true });
     cy.wait(300);
     cy.get('[data-testid="transactions-list"]').should('exist');
     cy.get('[data-testid="transaction-item"]').its('length').should('be.greaterThan', 0);
-    cy.wait(500);
+    cy.wait(delay_demo);
 
     cy.get('[data-testid="back-home"]').click();
     cy.wait(300);
